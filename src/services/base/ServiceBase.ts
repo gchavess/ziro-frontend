9db059247f8be2c6e833ffafd89bad7269ef6383
Gustavo Chaves <gustavchaves@hotmail.com>
2025-10-15 19:37:51 -0300

    fix: error cors for tests

--- src/services/base/ServiceBase.ts
+++ src/services/base/ServiceBase.ts
@@ -1,42 +1,76 @@
 import { authState } from "@/store/auth";
-import axios, { AxiosStatic } from "axios";
+import axios, {
+  AxiosError,
+  AxiosInstance,
+  AxiosRequestConfig,
+  AxiosResponse,
+} from "axios";
 
 export default class ServiceBase {
-  protected API_URL = import.meta.env.VITE_APP_URL_BACK_END;
+  protected readonly API_URL = import.meta.env.VITE_APP_URL_BACK_END;
+  protected readonly $axios: AxiosInstance;
 
   constructor() {
-    this.$axios.interceptors.request.use(
-      (config) => {
-        config.baseURL = this.API_URL;
+    this.$axios = axios.create({
+      baseURL: this.API_URL,
+      withCredentials: true,
+    });
 
-        config.withCredentials = true;
+    this.setupInterceptors();
+  }
 
-        config.headers["Authorization"] = `${authState.token}`;
-        config.headers["Access-Control-Allow-Origin"] = "*";
-
-        if (authState.usuarioLogadoId) {
-          config.headers["XXX-USUARIO-ID"] = authState.usuarioLogadoId;
-        }
-
-        return config;
-      },
-      function (error) {
-        return Promise.reject(error);
-      }
-    );
-
+  /**
+   * Configura interceptors de request e response.
+   */
+  private setupInterceptors() {
+    this.$axios.interceptors.request.use(this.onRequest, this.onRequestError);
     this.$axios.interceptors.response.use(
-      function (response) {
-        return response;
-      },
-      function (error) {
-        if (error.request.status == 403) {
-          window.top?.location.reload();
-        }
-        return Promise.reject(error);
-      }
+      this.onResponse,
+      this.onResponseError
     );
   }
 
-  $axios: AxiosStatic = axios;
+  /**
+   * Interceptor de request: adiciona headers de autenticação.
+   */
+  private onRequest(config: AxiosRequestConfig) {
+    if (!config.headers) config.headers = {};
+
+    if (authState.token) {
+      config.headers.Authorization = `Bearer ${authState.token}`;
+    }
+
+    if (authState.usuarioLogadoId) {
+      config.headers["XXX-USUARIO-ID"] = authState.usuarioLogadoId;
+    }
+
+    return config;
+  }
+
+  private onRequestError(error: AxiosError) {
+    console.error("Erro na requisição:", error);
+    return Promise.reject(error);
+  }
+
+  /**
+   * Interceptor de response: trata erros comuns de autenticação.
+   */
+  private onResponse(response: AxiosResponse) {
+    return response;
+  }
+
+  private onResponseError(error: AxiosError) {
+    const status = error.response?.status;
+
+    if (status === 401 || status === 403) {
+      const isOnLoginPage = window.location.pathname.includes("/login");
+      if (!isOnLoginPage) {
+        // Opcional: deslogar ou redirecionar
+        // authState.logout?.();
+        // window.location.href = "/login";
+      }
+    }
+
+    return Promise.reject(error);
+  }
 }
